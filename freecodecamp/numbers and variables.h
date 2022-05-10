b631756d93adfe850f4215027d1b97f47d34ee87
BenBenny11 <reaperbenny6@gmail.com>
2022-05-10 08:06:50 +0800

    added into numbers and variables

--- freecodecamp/numbers and variables.h
+++ freecodecamp/numbers and variables.h
@@ -4,6 +4,8 @@ static int a{ 1 }; //brace initialization
 static int b(2); //functional init
 static int c = 3; //assignment init
 
+static signed int negvalue = -300; //signed = modifier to store neg/pos values, unsigned to only store pos value
+
 //variables are case sensitive
 
 inline void thesizeof(int x) {
