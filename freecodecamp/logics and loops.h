a625542bb3b5de5ad06ae6654f8200090cfc405e
BenBenny11 <reaperbenny6@gmail.com>
2022-05-16 07:59:12 +0800

    Fixed logics and loops.h

---
+++ freecodecamp/logics and loops.h
@@ -0,0 +1,72 @@
+#pragma once
+#include "main.h"
+
+//ternary operator
+//can also do ternary initialization
+//int speed { (bool) ? 300 : 150};
+inline void ternaryop() {
+
+	int result{};
+	int tern1{ 10 };
+	int tern2{ 20 };
+	result = (tern1 > tern2) ? tern1 : tern2;
+	/*is also equivalent to
+	if (tern1 > tern2) {
+
+		result = tern1;
+
+	} else {
+
+		result = tern2;
+
+	}
+	*/
+
+}
+
+inline void whattool() {
+
+	const int pen{ 10 };
+	const int marker{ 20 };
+	const int eraser{ 30 };
+
+	int tool{ pen };
+
+	switch (tool) {
+
+	case pen: {
+		cout << "pen is active" << endl;
+
+	}
+			break;
+
+	case marker: {
+		cout << "marker is active" << endl;
+
+	}
+			   break;
+
+	case eraser: {
+		cout << "eraser is active" << endl;
+
+	}
+			   break;
+
+	default: {
+		cout << "no tool active" << endl;
+
+	}
+
+	}
+
+}
+
+//for loop
+inline void forloop() {
+
+	for (int i(); i < 10; i++) { //for (init variable; condition; what to do after each iteration)
+		cout << "this is the " << i << "th operation" << endl;
+
+	}
+
+}
