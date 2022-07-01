f9869926c44840c5e664ccbbf79e021b0e3ad1f5
BenBenny11 <reaperbenny6@gmail.com>
2022-07-01 13:55:35 +0800

    Added to cpp20 concepts.h

--- freecodecamp/cpp20 concepts.h
+++ freecodecamp/cpp20 concepts.h
@@ -29,3 +29,41 @@ template <typename T>
 T add(T a, T b) requires integral <T> { return a + b; }
 
 //enforce integral concept after function params list
+
+
+//build ur own concept
+//syntax 1
+
+template <typename T>
+concept MyIntegral = is_integral_v<T>;
+
+//syntax 2
+
+template <typename T>
+concept Multipliable = requires(T a, T b) { a* b; };
+
+//syntax 3
+
+template <typename T>
+concept Incrementable = requires(T a) {
+	a += 1;
+	++a;
+	a++;
+
+};
+
+//using custom concepts
+//syntax 1
+
+template <typename T>
+requires MyIntegral<T>
+T add_1(T a, T b) { return a + b; };
+
+//syntax 2
+
+template <MyIntegral T>
+T add_2(T a, T b) { return a + b; };
+
+//syntax 3
+
+auto add_3(MyIntegral auto a, MyIntegral auto b) { return a + b; };
