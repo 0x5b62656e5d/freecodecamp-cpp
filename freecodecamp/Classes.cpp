8e5cb9620a8c4452ec865e3c8da324a64da3943e
BenBenny11 <reaperbenny6@gmail.com>
2022-07-05 14:11:37 +0800

    Updated classes.cpp

--- freecodecamp/Classes.cpp
+++ freecodecamp/Classes.cpp
@@ -1,20 +1,6 @@
 #include "main.h"
 #include "Classes.h"
 
-cylinderB::cylinderB(double radius_param, double height_param) { //passed by values, values are copies of parameters
-	base_radius = radius_param;
-	height = height_param;
-};
-
-double cylinderB::volumeB() { return PI * base_radius * base_radius * height; }
-
-double cylinderB::get_radius() { return base_radius; }
-double cylinderB::get_height() { return height; }
-
-void cylinderB::set_radius(double radius_param) { base_radius = radius_param; }
-void cylinderB::set_height(double height_param) { height = height_param; }
-
-
 void usecylinderclass() {
 
 	Cylinder cylinder1;
@@ -34,7 +20,7 @@ void usearrowpointer() {
 
 	cylinderB* c1 = new cylinderB(); //create new object on heap
 
-	(*c1).set_radius(rad_param);
+	(*c1).set_radius(15.0);
 
 	//or
 
@@ -46,5 +32,45 @@ void usearrowpointer() {
 	delete c1;
 
 	cylinderB c2(10.0, 5.0);
+
+}
+
+cylinderB::cylinderB(double radius_param, double height_param) { //passed by values, values are copies of parameters
+	base_radius = radius_param;
+	height = height_param;
+};
+
+double cylinderB::volumeB() { return PI * base_radius * base_radius * height; }
+
+double cylinderB::get_radius() { return base_radius; }
+double cylinderB::get_height() { return height; }
+
+void cylinderB::set_radius(double radius_param) { base_radius = radius_param; }
+void cylinderB::set_height(double height_param) { height = height_param; }
+
+//destructors
+
+Dog::Dog() {
+
+	dog_name = "None";
+	dog_breed = "None";
+	dog_age = new int;
+	*dog_age = 0;
+
+}
+
+Dog::Dog(string_view name_param, string breed_param, int age_param) {
+
+	dog_name = name_param;
+	dog_breed = breed_param;
+	dog_age = new int;
+	*dog_age = age_param;
+
+}
+
+Dog::~Dog() {
+
+	delete dog_age;
+	cout << "Dog age destructor called for " << dog_name << endl;
 
 }
