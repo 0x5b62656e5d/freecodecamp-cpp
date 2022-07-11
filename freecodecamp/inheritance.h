6804a52b030b455244954d2f61ca6d69dc166213
BenBenny11 <reaperbenny6@gmail.com>
2022-07-11 20:44:26 +0800

    Revert "Revert "inheritance.h""
    This reverts commit 29120bf39b65e66d7e04bfe46c3ffb92c3880e77.

--- freecodecamp/inheritance.h
+++ freecodecamp/inheritance.h
@@ -1,3 +1,58 @@
 #pragma once
 #include "main.h"
 
+//other functions (ignore)
+
+
+
+//building class on top of other classes
+
+class Person {
+
+	friend ostream& operator<< (ostream& out, const Person& person);
+
+public:
+	Person() = default;
+	Person(string& fname_param, string& lname_param);
+	~Person();
+
+	const string get_fname() const { return first_name; }
+	const string get_lname() const { return last_name; }
+
+private:
+	string last_name{ "Mysterious" };
+	string first_name{ "Person" };
+
+};
+
+class Player : public Person { //syntax to derive from class
+
+	friend ostream& operator<< (ostream& out, const Player& player);
+
+public:
+	Player() = default;
+	Player(string_view game_param);
+	~Player();
+
+private:
+	string m_game{ "None" };
+
+};
+
+
+
+class test {
+	
+private:
+	int testint{ 0 };
+
+public:
+
+	test() = default;
+	test(int num) { testint = num; }
+
+	void reset(test& test1) { testint = 0; }
+
+	friend void reset2(test& test1) { testint = 0; }
+
+};
