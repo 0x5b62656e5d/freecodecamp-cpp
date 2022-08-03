09a5575dc720208dfeaf1717030c7ee9002882bc
BenBenny11 <reaperbenny6@gmail.com>
2022-08-03 18:25:49 +0800

    Added inheritance2 files

--- freecodecamp/inheritance2.cpp
+++ freecodecamp/inheritance2.cpp
@@ -1,2 +1,64 @@
 #include "inheritance2.h"
 
+//custom constructors in inheritance
+
+ostream& operator<<(ostream& out, const Person2& Person2) {
+
+	out << "Person [" << Person2.m_fullname << "] Age ["
+		<< Person2.m_age << "] Address [" << Person2.m_address
+		<< "]" << endl;
+
+	return out;
+
+}
+
+ostream& operator<<(ostream& out, const Engineer& Engineer) {
+
+	out << "Person [" << Engineer.get_fullname() << "] Age ["
+		<< Engineer.get_age() << "] Address [" << Engineer.get_address()
+		<< "] Contract count [" << Engineer.contractcount << "]"
+		<< endl;
+
+	return out;
+}
+
+ostream& operator<<(ostream& out, const CivilEngineer& operand) {
+
+	out << "Person [" << operand.get_fullname() << "] Age ["
+		<< operand.get_age() << "] Address [" << operand.get_address()
+		<< "] Contract count [" << operand.get_contract()
+		<< "] Speciality [" << operand.m_speciality << "]" << endl;
+
+	return out;
+}
+
+Person2::Person2(string_view fullname, int age, string_view address)
+	: m_fullname(fullname), m_age(age), m_address(address)
+{}
+
+Engineer::Engineer(string_view fullname, int age, 
+	string_view address, int contractcount)
+
+	: Person2(fullname, age, address), contractcount(contractcount)
+{}
+
+CivilEngineer::CivilEngineer(string_view fullname, int age,
+	string_view address, int contractcount, string_view speciality)
+
+	: Engineer(fullname, age, address, contractcount), m_speciality(speciality)
+{}
+ 
+
+//copy constructors in inheritance
+
+Engineer2::Engineer2(const Engineer2& source)
+	: Person2(source), contractcount(source.contractcount)
+{}
+
+CivilEngineer2::CivilEngineer2(const CivilEngineer2& source)
+	: Engineer2(source), m_speciality(source.m_speciality)
+{}
+
+Engineer3::Engineer3(const Engineer3& source)
+	: Person2(source), contractcount(source.contractcount)
+{}
--- freecodecamp/inheritance2.h
+++ freecodecamp/inheritance2.h
@@ -1,8 +1,125 @@
 #pragma once
 #include "main.h"
 
-class inheritance2 {
+//custom constructors in inheritance
 
+class Person2 {
 
+	friend ostream& operator<<(ostream& out, const Person2& Person2);
+
+public:
+	Person2() = default;
+	Person2(string_view fullname, int age, string_view address);
+	~Person2() = default;
+
+public:
+	string m_fullname{ "None" };
+
+	string get_fullname() const { return m_fullname; }
+	int get_age() const { return m_age; }
+	string get_address() const { return m_address; }
+	
+protected:
+	int m_age{ 0 };
+
+private:
+	string m_address{ "None" };
 
 };
+
+class Engineer : public Person2 {
+
+	friend ostream& operator<<(ostream& out, const Engineer& Engineer);
+
+public:
+	Engineer() = default;
+	Engineer(string_view fullname, int age, 
+		string_view address, int contractcount);
+	~Engineer() = default; 
+
+	int get_contract() const { return contractcount; }
+
+private:
+	int contractcount{ 0 };
+
+};
+
+class CivilEngineer : public Engineer {
+
+	friend ostream& operator<<(ostream& out, 
+		const CivilEngineer& CivilEngineer);
+
+public:
+	CivilEngineer() = default;
+	CivilEngineer(string_view fullname, int age,
+		string_view address, int contractcount, string_view speciality);
+	~CivilEngineer() = default;
+
+	string get_speciality() const { return m_speciality; }
+
+private:
+	string m_speciality{ "None" };
+
+};
+
+
+//copy constructors in inheritance
+
+class Engineer2 : public Person2 {
+
+	friend ostream& operator<<(ostream& out, const Engineer& Engineer);
+
+public:
+	Engineer2() = default;
+	Engineer2(const Engineer2& source);
+	~Engineer2() = default;
+
+	int get_contract() const { return contractcount; }
+
+private:
+	int contractcount{ 0 };
+
+};
+
+class CivilEngineer2 : public Engineer2 {
+
+	friend ostream& operator<<(ostream& out,
+		const CivilEngineer& CivilEngineer);
+
+public:
+	CivilEngineer2() = default;
+	CivilEngineer2(const CivilEngineer2& source);
+	~CivilEngineer2() = default;
+
+	string get_speciality() const { return m_speciality; }
+
+private:
+	string m_speciality{ "None" };
+
+};
+
+//inheriting base constructors
+
+class Engineer3 : public Person2 {
+
+	using Person2::Person2;
+
+	friend ostream& operator<<(ostream& out, const Engineer& Engineer);
+
+public:
+	Engineer3(const Engineer3& source);
+	~Engineer3() = default;
+
+	int get_contract() const { return contractcount; }
+
+private:
+	int contractcount{ 0 };
+
+};
+
+//compiler generated constructor will look like this:
+/*
+Engineer3(const string& fullname, int age, const string adderss) 
+	: Person(fullname, age, address)
+{}
+*/
