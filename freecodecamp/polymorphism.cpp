1571ee1dba2d232a0820789133d0e45249e40493
BenBenny11 <reaperbenny6@gmail.com>
2022-08-09 20:23:08 +0800

    Added polymorphism code

--- freecodecamp/polymorphism.cpp
+++ freecodecamp/polymorphism.cpp
@@ -1,1 +1,100 @@
 #include "polymorphism.h"
+
+Shape::Shape(const string_view desc) : m_desc(desc) {}
+
+
+Oval::Oval(double x_rad, double y_rad, const string_view desc)
+	: m_x_rad(x_rad), m_y_rad(y_rad), Shape(desc)
+{}
+
+
+Circle::Circle(double rad, const string_view desc)
+	: Oval(rad, rad, desc) {}
+
+Circle2::Circle2(double rad, const string_view desc)
+	: Oval(rad, rad, desc) {}
+
+//static binding with base class ptr
+
+void staticbind() {
+
+	Shape shape1("Shape1");
+	Oval oval1(2, 3.5, "Oval1");
+	Circle circle1(3.3, "Circle1");
+
+	cout << "Calling methods thru ptrs - static binding" << endl;
+
+	Shape* shape_ptr = &shape1;
+
+	shape_ptr->draw();
+	
+	shape_ptr = &oval1;
+	shape_ptr->draw(); //will not call oval::draw but shape::draw because 
+						//draw function is statically bound to shape ptr
+	shape_ptr = &circle1;
+	shape_ptr->draw();
+
+	delete shape_ptr;
+	shape_ptr = nullptr;
+
+}
+
+void dynamicbind() {
+
+	//put "virtual" keyword in front of function used in polymorphism
+
+	Shape shape1("Shape1");
+	Oval oval1(2, 3.5, "Oval1");
+	Circle circle1(3.3, "Circle1");
+
+	Shape* shape_ptr = &shape1;
+	shape_ptr->draw();
+
+	shape_ptr = &oval1;
+	shape_ptr->draw();
+
+	shape_ptr = &circle1; //will now do shape draw, oval draw, circle draw
+	shape_ptr->draw();	//since functions are now marked as virtual
+
+	delete shape_ptr;
+	shape_ptr = nullptr;
+
+	Shape* shape_collec[]{ &shape1, &oval1, &circle1 };
+	for (Shape* s_ptr : shape_collec) {
+		s_ptr->draw();
+
+	}
+
+}
+
+void draw_shape(Shape* s) { //dynamioc binding
+	s->draw(); //can pass circle, shape, or oval and will use 
+	//the method from object passed
+}
+
+void polymorphicarray() {
+
+	Circle circle1(7.2, "circle1");
+	Oval oval1(13.3, 1.2, "Oval1");
+	Circle circle2(11.2, "circle2");
+	Oval oval2(31.3, 15.2, "oval2");
+	Circle circle3(12.2, "circle3");
+	Oval oval3(53.3, 9.2, "Oval3");
+
+	Shape* shapes[]{ &circle1, &oval1, &circle2, &oval2, &circle3, &oval3 };
+
+	for (Shape* s : shapes) {
+		s->draw();
+
+	}
+
+	//smart pointers
+
+	shared_ptr<Shape> shapes2[]{ make_shared<Circle>(12.2, "Circle4"), make_shared<Oval>(10.0, 20.0, "oval4") };
+
+	for (auto& s : shapes) {
+		s->draw();
+
+	}
+
+}
--- freecodecamp/polymorphism.h
+++ freecodecamp/polymorphism.h
@@ -1,2 +1,84 @@
 #pragma once
 #include "main.h"
+
+class Shape {
+
+public:
+	Shape() = default;
+	Shape(const string_view desc);
+	virtual ~Shape() = default;
+
+	virtual void draw() const {
+		cout << "Shape::draw() called. Drawing " << m_desc << endl;
+
+	}
+
+protected:
+	string m_desc{ "" };
+
+};
+
+class Oval : public Shape {
+
+public:
+	Oval() = default;
+	Oval(double x_rad, double y_rad, const string_view desc);
+	virtual ~Oval() = default;
+
+	virtual void draw() const {
+		cout << "Oval::draw() called. Drawing " << m_desc << " with radius x "
+			<< m_x_rad << " and radius y " << m_y_rad << endl;
+
+	}
+
+protected:
+	double get_x_rad() const { return m_x_rad; }
+	double get_y_rad() const { return m_y_rad; }
+
+private:
+	double m_x_rad{ 0.0 };
+	double m_y_rad{ 0.0 };
+
+};
+
+class Circle : public Oval {
+
+public:
+	Circle() = default;
+	Circle(double rad, const string_view desc);
+	virtual ~Circle() = default;
+
+	virtual void draw() const override {
+		cout << "Circle::draw() called. Drawing " << m_desc
+			<< " with radius " << get_x_rad() << endl;
+
+	}
+
+	//add override keyword after const keyword
+	//used to check if functions in derived classes have same 
+	//signature as parent/base class
+
+};
+
+//final specifier (can put in overridden functions or derived class definition)
+//if function marked as final, derived classes won't be 
+//able to override function - will have to use implementation of parent class
+//if class marked final, class cannot be derived further
+
+class Circle2 final : public Oval {
+
+public: 
+	Circle2() = default;
+	Circle2(double rad, const string_view desc);
+	virtual ~Circle2() = default;
+
+	virtual void draw() const final{
+		cout << "Circle::draw() called. Drawing " << m_desc
+			<< " with radius " << get_x_rad() << endl;
+
+	}
+
+};
+
+//add virtual specifier to destructor will properly delete objects
+//if base pointers were used to manage derived objects
