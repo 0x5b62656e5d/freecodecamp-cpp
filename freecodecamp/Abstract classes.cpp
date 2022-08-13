a555c5530f4a6c7adb48f2413fa12a5b2e1f1bfa
BenBenny11 <reaperbenny6@gmail.com>
2022-08-13 15:40:32 +0800

    Final commit - added abstract classes and interfaces

--- freecodecamp/Abstract classes.cpp
+++ freecodecamp/Abstract classes.cpp
@@ -1,2 +1,33 @@
 #include "Abstract classes.h"
 
+Shape::Shape(string_view desc)
+	: m_desc(desc) {}
+
+Circle3::Circle3(double rad, string_view desc)
+	: m_rad(rad), Shape(desc) {}
+
+Rectangle::Rectangle(double width, double height, string_view desc)
+	: m_width(width), m_height(height), Shape(desc) {}
+
+void usetypeid() {
+
+	const Shape* shape_rect = new Rectangle(10, 10, "Rectangle1");
+
+	double surface = shape_rect->area();
+
+	cout << "dynamic type of shape_rect: " 
+		<< typeid(*shape_rect).name() << endl; 
+	//returns type of object (rectangle, shape, circle)
+
+	cout << "Area of shape_rect: " << surface << endl;
+
+	delete shape_rect;
+
+}
+
+//interface
+ostream& operator<< (ostream& out, const StreamInsertable& operand) {
+	operand.stream_insert(out);
+	return out;
+
+}
--- freecodecamp/Abstract classes.h
+++ freecodecamp/Abstract classes.h
@@ -1,3 +1,101 @@
 #pragma once
 #include "main.h"
 
+class Shape { //shape will become abstract
+
+protected:
+	Shape() = default;
+	Shape(string_view desc);
+	
+public:
+	virtual ~Shape() = default;
+	//even if class is abstract, destructor has to be public
+	//to delete pointer objects
+
+	//pure virtual functions (abstract)
+	//implementations of function will be defined in derived classes
+	virtual double perimeter() const = 0;
+	virtual double area() const = 0;	
+
+private:
+	string m_desc{};
+
+};
+
+class Circle3 : public Shape {
+
+public:
+	Circle3() = default;
+	Circle3(double rad, string_view desc);
+	virtual ~Circle3() = default;
+
+	virtual double perimeter() const override {
+		return (2 * m_rad * m_pi);
+
+	}
+
+	virtual double area() const override {
+		return (m_rad * m_rad * m_pi);
+
+	}
+
+private:
+	double m_rad{ 0.0 };
+
+	const double m_pi{ 3.141592653589793238462643383279502884L };
+
+};
+
+class Rectangle : public Shape {
+
+public:
+	Rectangle() = default;
+	Rectangle(double width, double height, string_view desc);
+	virtual ~Rectangle() = default;
+
+	virtual double perimeter() const override {
+		return (2 * m_width + 2 * m_height);
+
+	}
+
+	virtual double area() const override {
+		return (m_width * m_height);
+
+	}
+
+private:
+	double m_width{ 0.0 };
+	double m_height{ 0.0 };
+
+};
+
+//interfaces
+//abstract classes with only functions and no member variables
+
+class StreamInsertable {
+
+	friend ostream& operator<< (ostream& out, 
+		const StreamInsertable& operand);
+
+public:
+	virtual void stream_insert(ostream& out) const = 0;
+
+};
+
+class Point : public StreamInsertable {
+
+public:
+	Point() = default;
+	Point(double x, double y)
+		: m_x(x), m_y(x) {}
+
+	virtual void stream_insert(ostream& out) const override {
+		out << "Point (" << m_x << ", " << m_y << ")" << endl;
+
+	}
+
+private:
+	double m_x{};
+	double m_y{};
+
+};
